68753c0f359831d42ed94b2eb5a7345b0246cba8
Пацвальд Владимир <vova.patsvald@mail.ru>
2024-09-07 15:43:19 +1000

    fix: bags3

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 from .models import Post, Location, Category
+from django.contrib.auth.admin import UserAdmin
+
+from .models import MyUser
+
+UserAdmin.fieldsets += (
+    ('Extra Fields', {'fields': ('bio',)}),
+)
+
+admin.site.register(MyUser, UserAdmin)
 
 
 @admin.register(Category)
